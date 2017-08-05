fab0de5625ea2a111fd1cf8dc53695cd3e5d1f10
mkapiczy <mix.kap@gmail.com>
2017-08-05 14:48:10 +0200

    FacebookController rename to OAuth Controller

--- src/main/java/com/github/britter/springbootherokudemo/endpoint/OAuthController.java
+++ src/main/java/com/github/britter/springbootherokudemo/endpoint/OAuthController.java
@@ -1,18 +1,3 @@
-/*
- * Copyright 2015 Benedikt Ritter
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.github.britter.springbootherokudemo.endpoint;
 
 import com.github.britter.springbootherokudemo.FacebookRepository;
@@ -27,16 +12,16 @@ import javax.servlet.http.HttpServletResponse;
 
 @Controller
 @RequestMapping("/oauth")
-public class FacebookController {
+public class OAuthController {
 
     private FacebookRepository repository;
-    private String clientId = "1955772338033020";
+    private String clientId = "qtbmgp9cl9vtel86fcgiuicfol";
     String appDomain = "http://localhost:8080";
     private String redirectUri = appDomain + "/oauth/handleRedirect";
-    private String OAuthAuthorizationUrl = "http://www.localhost:8090/oauth/authenticate?client_id=%s&redirect_uri=%s";
+    private String OAuthAuthorizationUrl = "http://localhost:8090/oauth/authenticate?client_id=%s&redirect_uri=%s";
 
     @Autowired
-    public FacebookController(FacebookRepository repository) {
+    public OAuthController(FacebookRepository repository) {
         this.repository = repository;
     }
 
